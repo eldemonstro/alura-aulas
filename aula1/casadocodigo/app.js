da16c050a34abc174fdbef64a950ebc76d5de23a
Fabricio <demonstro003@gmail.com>
2017-04-05 14:14:48 -0300

    aula 1 atividade 12

--- aula1/casadocodigo/app.js
+++ aula1/casadocodigo/app.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/produtos', function(req, res){
-    res.send("<html><body><h1>Lista De Produtos</h1></body></html>");
+    res.render("produtos/lista");
 });
 
 app.listen(3000, function(){
