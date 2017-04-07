4901c2b79cb4dd2504b46ad381b6ef375fdac2cb
Fabricio Bezerra <Fabricio Bezerra>
2017-04-07 13:04:52 -0300

    voltando ao node js aula 02 atividade 13

--- aulasnode/casadocodigo/app/routes/produtos.js
+++ aulasnode/casadocodigo/app/routes/produtos.js
@@ -1,16 +1,10 @@
+var connectionFactory = require('../infra/connectionFactory');
 module.exports = function (app) {
     app.get('/produtos', function (req, res) {
         //conectando no mysql
-        var mysql = require('mysql');
-        var connection = mysql.createConnection({
-            host : 'localhost',
-            user : 'root',
-            password : 'admin',
-            database : 'casadocodigo_nodejs'
-        });
+        var connection = connectionFactory();
 
         connection.query('select * from livros', function(err, result){
-            console.log(result[0].id);
             res.render('produtos/lista', {lista:result});
         });
         connection.end();
