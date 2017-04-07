ddf13c5e899656c4bcfa53624c33c433342215a8
Fabricio Bezerra <Fabricio Bezerra>
2017-04-07 16:06:55 -0300

    nem lembro

--- aulasnode/casadocodigo/app/routes/produtos.js
+++ aulasnode/casadocodigo/app/routes/produtos.js
@@ -1,11 +1,26 @@
 module.exports = function (app) {
     app.get('/produtos', function (req, res) {
         var connection = app.infra.connectionFactory();
-        var produtosBanco = new app.infra.ProdutosDAO(connection);
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
 
-        produtosBanco.lista(function (erros, resultados) {
+        produtosDAO.lista(function (erros, resultados) {
             res.render('produtos/lista', { lista: resultados });
         });
         connection.end();
+    });
+
+    app.get('/produtos/form', function (req, res) {
+        res.render('produtos/form');
+    });
+
+    app.post('/produtos', function (req, res) {
+
+        var produto = req.body;
+
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.salva(produto, function (err, results) {
+            res.redirect('/produtos');
+        });
     });
 }
