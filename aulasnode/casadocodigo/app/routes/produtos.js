7f2382daedfee4e79c3d550ff1ea285f2b88f2a4
Fabricio Bezerra <Fabricio Bezerra>
2017-04-07 22:01:38 -0300

    AULA 666666666666 de node

--- aulasnode/casadocodigo/app/routes/produtos.js
+++ aulasnode/casadocodigo/app/routes/produtos.js
@@ -4,18 +4,42 @@ module.exports = function (app) {
         var produtosDAO = new app.infra.ProdutosDAO(connection);
 
         produtosDAO.lista(function (erros, resultados) {
-            res.render('produtos/lista', { lista: resultados });
+            res.format({
+                html: function () {
+                    res.render('produtos/lista', { lista: resultados });
+                },
+                json: function(){
+                    res.json(resultados);
+                }
+            });
         });
         connection.end();
     });
 
     app.get('/produtos/form', function (req, res) {
-        res.render('produtos/form');
+        res.render('produtos/form', {errosValidacao: {}, produto: {}});
     });
 
     app.post('/produtos', function (req, res) {
 
         var produto = req.body;
+
+        req.assert('titulo', 'Titulo  é obrigatorio').notEmpty();
+        req.assert('descricao', 'Descrição  é obrigatoria').notEmpty();
+        req.assert('preco', 'Formato invalido').isFloat();
+
+        var erros = req.validationErrors();
+        if (erros) {
+            res.format({
+                html: function () {
+                    res.status(400).render('produtos/form', {errosValidacao: erros, produto: produto});
+                },
+                json: function(){
+                    res.status(400).json(erros);
+                }
+            });
+            return;
+        }
 
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
