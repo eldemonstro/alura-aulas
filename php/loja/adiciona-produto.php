ff36544ac9dc5b365e42590b4686d904dffaebfd
Fabricio Bezerra <demonstro003@gmail.com>
2017-05-21 11:28:41 -0300

    php aula 9

--- php/loja/adiciona-produto.php
+++ php/loja/adiciona-produto.php
@@ -4,10 +4,12 @@
   include 'banco-produto.php';
 
 
-  $nome = $_GET["nome"];
-  $preco = $_GET["preco"];
+  $nome = $_POST["nome"];
+  $preco = $_POST["preco"];
+  $descricao = $_POST["descricao"];
+  $categoria_id = $_POST["categoria_id"];
 
-  if(insereProduto($conexao, $nome, $preco)) { ?>
+  if(insereProduto($conexao, $nome, $preco, $descricao, $categoria_id)) { ?>
     <p class="text-success">Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
     <?php
   } else {
--- php/loja/produto-formulario.php
+++ php/loja/produto-formulario.php
@@ -1,6 +1,11 @@
-<?php include 'cabecalho.php'; ?>
+<?php
+  include 'cabecalho.php';
+  include 'conecta.php';
+  include 'banco-categoria.php';
+  $categorias = listaCategorias($conexao);
+?>
   <h1>Formulario de produto</h1>
-  <form action="adiciona-produto.php">
+  <form action="adiciona-produto.php" method="post">
     <div class="form-group">
       <label for="nome">Nome:</label>
       <input class="form-control" type="text" name="nome" value="carro"><br>
@@ -12,6 +17,17 @@
     <div class="form-group">
       <label for="descricao">Descrição:</label>
       <textarea name="descricao" class="form-control" rows="4"></textarea>
+    </div>
+    <label>Categoria: </label>
+    <div class="form-group">
+      <?php foreach($categorias as $categoria) : ?>
+      <div class="radio-inline">
+          <label>
+            <input type="radio" name="categoria_id" value="<?=$categoria['id']?>">
+            <?=$categoria['nome']?>
+          </label>
+        </div>
+      <?php endforeach ?>
     </div>
     <button class="btn btn-primary" type="submit" name="submit">Cadastrar</button>
   </form>
